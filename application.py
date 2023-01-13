4f6ad28d0c9e203be199bac5399d4af8f0da319a
Bill Lan <bilan604gm@gmail.com>
2023-01-13 12:33:01 -0800

    Git?

--- application.py
+++ application.py
@@ -315,9 +315,7 @@ global bot_commands
 bot_commands = {command.name: command for command in list(bot.commands)}
 
 
-from threading import Thread 
-from functools import partial 
-from webApp import initializeWebApp
+
 
   
 def runApp(__name__):
--- webApp.py
+++ webApp.py
@@ -10,30 +10,32 @@ from hosting.FlaskServer import askOpenAI
 def initializeWebApp(__name__):
   from handling.chatGPT import generate_response
 
-  appx = Flask(__name__)
+  app = Flask(__name__)
   openai.api_key = os.getenv("OPENAI_API_KEY")
-  print("App created!")
 
-  @appx.route("/", methods=("GET", "POST"))
+  print("App created!")
+  @app.route("/", methods=("GET", "POST"))
   def index():
       if request.method == "POST":
         query = request.form["query"]
+        """
         response = openai.Completion.create(
             model="text-davinci-002",  # 002
             prompt=generate_response(query),
             temperature=0.6,
             max_tokens=850
         )
+        """
+        response = "Your query was: " + query
         return redirect(url_for("index", result=response.choices[0].text))
 
       # "GET" request: Update the HTML page to display the response
       result = " "
       
-      # make connect 4!
       result_test = "Show ConnectFour"
-
 
       # HTML will check if result_string is not null and if so, 
       return render_template("index.html", result=result, result_test=result_test)  # re-renders the element
 
-  return appx
+
+  return app
